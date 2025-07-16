b69d1c62a839b021ce206b428f3670060beaba9f
Almuheetu shihab <almuheetu@gmail.com>
2025-07-16 20:25:31 +0600

    Documents: incuding some basic & fundamenta implement

---
+++ fundamental.js
@@ -0,0 +1,17 @@
+// How to declear a variable using `let` and const
+
+const fatherName = "John"; // This is a constant variable
+let season = "rainy"; // This is a variable that can be reassigned
+
+
+// 6 basic conditional statements : >, <, >=, <=, ===, !==
+// multiple conditions can be combined using logical operators like && (AND) and || (OR)
+
+if(fatherName === "John" && season === "rainy") {
+    console.log("It's rainy season and the father's name is John.");
+}
+
+if(fatherName === "John" || season === "rainy") {
+    console.log("Either it's rainy season or the father's name is John.");
+}
+
